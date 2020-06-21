89134a22cdd42ff6e35b38ecf8c47c9a64ca3e7b
Leonardo Setubal <setubal43@gmail.com>
2020-06-20 23:31:54 -0300

    feat: adicionadoa arquivo para rotas

--- src/config/enviroments.ts
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-export const db = {
-	uri: process.env.DB_URI || ''
-}
-
-export const port_serve = process.env.PORT_SERVER || 3333
-export const secret = process.env.SECRET || ''
---
+++ src/routes/routes.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const routes = express.Router()
+
+//Add your routes here
+
+
+
+
+
+export default routes
